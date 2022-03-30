f32ff861bf6825c017785cd8b1edd89f8c50fbb2
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-03-30 02:42:41 +0200

    add top and bottom pane, and navbar
    add header with 17.5% height, logo, search bar, and simple navbar

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import logo from '../logo.svg';
+
+function Header() {
+
+  const navItems = ['Smartphones', 'Tablets', 'Wearables', 'Electronics', 'Appliances']
+
+  return (
+    <div>
+      <div className='top-pane'>
+        <div className='top-pane_info'>
+          <p>Fast & Free Delivery</p>
+          <p>Shop though a wide range of products</p>
+          <p>Free Returns</p>
+        </div>
+      </div>
+
+      <div className='bottom-pane'>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-tittle">E-Shop</h1>
+        <input className="header-search"></input>
+        <h3 className='right-pane'>Deals</h3>
+        <h3 className='right-pane'>Account</h3>
+      </div>
+
+      <div className="navbar">
+        {navItems.map(i => {
+          return (
+            <h4 className="navbar-item">{i}</h4>
+          )
+        })}
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
