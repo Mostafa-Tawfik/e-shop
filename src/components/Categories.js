bdf939d089b8cef0599c51bede7e09ac0e23581c
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-04-01 16:01:24 +0200

    add dynamic categories to homepage

---
+++ src/components/Categories.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from 'react'
+import AppData from '../AppData'
+
+function Categories() {
+  
+  return (
+    <div className='cat-holder'>
+      {AppData.categories.map(d => {
+        return (
+          <Fragment key={d.id}>
+            <h4>{d.title}</h4>
+
+            <div className='product-holder'>
+            {d.items.map(i => {
+              return (
+                  <div key={i.id}  className='product-card'>
+                    <div className='discount'>
+                      <h5>{i.discount}</h5>
+                    </div>
+
+                    <img src={i.image} alt='product'  className='product-img'></img>
+
+                    <div className='product-info'>
+                      <h4 className='product-title'>{i.title}</h4>
+
+                      {i.discountprice ? 
+                      <div className='product-price-com'>
+                        <h5 className='product-newprice'>{i.discountprice}</h5>
+                        <h5 className='product-oldprice'>{i.price}</h5>
+                      </div>
+                       : 
+                      <h5 className='product-price'>{i.price}</h5>}
+                    </div>
+                    
+
+                    <button>
+                      <img src='https://api.iconify.design/bi/cart-plus-fill.svg?color=%23073c81' alt='add-to-cart'></img>
+                    </button>
+                  </div>
+              )
+            })}
+            </div>
+          </Fragment>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Categories
