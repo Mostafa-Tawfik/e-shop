29a0eb1032d76d5d1ad2e79f141a570ffb8de870
homie980 <boodykhaled@gmail.com>
2022-04-16 17:26:05 +0200

    Finished Product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,10 +2,20 @@ const express = require("express");
 const {
   getProducts,
   createProduct,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+  createProductReview,
 } = require("../controllers/productController");
 const router = express.Router();
 const { protect, admin } = require("../middleware/authMiddleware");
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 module.exports = router;
