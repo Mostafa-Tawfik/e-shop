77e1f3b5ac9581558dc0191cb2a13cf5e5c18196
homie980 <boodykhaled@gmail.com>
2022-04-23 22:25:58 +0200

    Fix for deployment v2

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+const path = require("path");
 
 connectDB();
 
@@ -18,5 +19,17 @@ app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/upload", require("./routes/uploadRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/complaints", require("./routes/complaintRoutes"));
+
+// Serve the frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
