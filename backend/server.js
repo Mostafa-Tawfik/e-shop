0bbb1d31d61cfde2a89866c35ed7b21d7989003e
homie980 <boodykhaled@gmail.com>
2022-04-23 22:15:18 +0200

    Fix for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
