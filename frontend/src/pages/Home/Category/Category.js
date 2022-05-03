8f312b082271213382a5c019a04110df85ff731e
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-05-04 01:31:51 +0200

    fix filter by rating on category section

--- frontend/src/pages/Home/Category/Category.js
+++ frontend/src/pages/Home/Category/Category.js
@@ -1,45 +1,63 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 
 import ProductCard from '../../../components/ProductCard'
 import Ratings from 'react-ratings-declarative'
+import useProducts from '../../../hooks/useProducts'
 
 function Category(props) {
 
   const params = useParams()
 
   // store all products
-  const [products, setProducts] = React.useState([])
-  
-  // fetch all products
-  useEffect(()=> {
-    axios.get('/api/products?productNum=Infinity')
-    .then(res => setProducts(res.data.products))
-  },[])
+  const {data: products} = useProducts()
 
-  const filterByCategory = products.filter(p=> p.category === params.name || p.subCategory === params.name || p.brand === params.name || p.rating >= params.name)
+  const categories = products && [...new Set(products.map(p => p.category))]
+
+  const [rating, setRating] = useState('')
+  console.log(rating);
+
+  let filterBy = products && products.filter(
+    p=> 
+    (p.category === params.name && p.rating >= rating) || 
+    (p.subCategory === params.name && p.rating >= rating) || 
+    (p.brand === params.name && p.rating >= rating) || 
+    (p.rating >= params.name && p.rating >= rating)
+  )
 
 
   // map over categories and return only unique values
-  const subCatWithinCat = [...new Set(filterByCategory.map(p => p.subCategory).filter(c => c !== undefined))]
+  const subCatWithinCat = products && [...new Set(filterBy.map(p => p.subCategory).filter(c => c !== undefined))]
 
   // map over brands and return only unique values
-  const brandWithinCat = [...new Set(filterByCategory.map(p => p.brand).filter(c => c !== undefined))]
+  const brandWithinCat = products && [...new Set(filterBy.map(p => p.brand).filter(c => c !== undefined))]
 
 
-  console.log(filterByCategory)
+  // console.log(filterBy)
   
   
   return (
-
+    products &&
     <main className='category'>
 
       <aside className='category-menu-holder'>
 
         <div className='category-menu dep'>
           <h4>Department</h4>          
+          <ul>
+          {categories.map((Cat, index) => (
+            <li key={index}>
+              <Link to={`/${Cat}`}>
+              {Cat}
+              </Link>
+            </li>                  
+              ))}
+          </ul>
+        </div>
+
+        <div className='category-menu dep'>
+          <h4>Sub Categories</h4>          
           <ul>
           {subCatWithinCat.map((subCat, index) => (
             <li key={index}>
@@ -68,8 +86,7 @@ function Category(props) {
           <h4>Average Reviews</h4>
           <ul>
 
-            <li>
-              <Link to={`/4`}>
+            <li onClick={()=>setRating(4)}>
                 <Ratings
                   rating={4}
                   widgetDimensions="20px"
@@ -82,11 +99,9 @@ function Category(props) {
                   <Ratings.Widget />
                   <Ratings.Widget />
                 </Ratings>
-              </Link>
             </li>
 
-            <li>
-              <Link to={`/3`}>
+            <li onClick={()=>setRating(3)}>
                 <Ratings
                   rating={3}
                   widgetDimensions="20px"
@@ -99,11 +114,9 @@ function Category(props) {
                   <Ratings.Widget />
                   <Ratings.Widget />
                 </Ratings>
-              </Link>                  
             </li>
 
-            <li>
-              <Link to={`/2`}>
+            <li onClick={()=>setRating(2)}>
                 <Ratings
                   rating={2}
                   widgetDimensions="20px"
@@ -116,11 +129,9 @@ function Category(props) {
                   <Ratings.Widget />
                   <Ratings.Widget />
                 </Ratings>                
-              </Link>
             </li>   
 
-            <li>
-              <Link to={`/1`}>
+            <li onClick={()=>setRating(1)}>
                 <Ratings
                   rating={1}
                   widgetDimensions="20px"
@@ -133,7 +144,21 @@ function Category(props) {
                   <Ratings.Widget />
                   <Ratings.Widget />
                 </Ratings>
-              </Link>
+            </li>         
+
+            <li onClick={()=>setRating(0)}>
+                <Ratings
+                  rating={0}
+                  widgetDimensions="20px"
+                  widgetSpacings="0px"
+                  widgetRatedColors="gold"
+                >
+                  <Ratings.Widget />
+                  <Ratings.Widget />
+                  <Ratings.Widget />
+                  <Ratings.Widget />
+                  <Ratings.Widget />
+                </Ratings>
             </li>                  
           </ul>
         </div>
@@ -145,7 +170,7 @@ function Category(props) {
         <h3>{isNaN(params.name) ? params.name : `Average rating more than ${params.name}`}</h3>
 
         <div className='product-holder'>
-          {filterByCategory.map(product => (
+          {filterBy.map(product => (
             <ProductCard content={product} {...props} key={product._id}/>
           ))}
         </div>
