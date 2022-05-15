39eea350a8107f50f2537f6d1058f0528ccb15a5
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-05-15 04:16:13 +0200

    install apexcharts and init a sales chart

--- frontend/src/pages/Dashboard/Home/Home.js
+++ frontend/src/pages/Dashboard/Home/Home.js
@@ -1,12 +1,58 @@
 import React from 'react'
+import Chart from "react-apexcharts"
+import useApi from '../../../hooks/useApi'
+
 import Statistics from '../Statistics/Statistics'
 
 function Home() {
 
+  // fetch orders
+  const {data: orders} = useApi('/api/orders', 'GET')
+  // console.log(orders);
+
+  const seriesData = orders && orders.map(order => ([
+    Date.parse(order.createdAt), Number(order.totalPrice).toFixed(0)
+  ]))
+
+  // sales chart
+  const salesChart = {
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        type: 'datetime'
+      }
+    },
+    series: [
+      {
+        name: "Sales",
+        data: seriesData
+      }
+    ]
+  }
+  
+
   return (
-    <div className='dashboard-section'>
-      <Statistics />
-    </div>
+    seriesData && 
+      <div className='dashboard-section'>
+  
+        <Statistics />
+  
+        <article>
+  
+          <h3>Sales</h3>
+  
+          <Chart
+            options={salesChart.options}
+            series={salesChart.series}
+            type="bar"
+            width="100%"
+          />
+  
+        </article>
+  
+      </div>    
   )
 }
 
